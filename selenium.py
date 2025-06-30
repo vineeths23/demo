7f0dd4e9c8559dbc476f44e28d173e6071efe9ed
vineeths23 <vineeths2323@gmail.com>
2025-06-30 18:59:54 +0530

    first Commit

---
+++ selenium.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+chrome_driver_path = "C:/driver/chromedriver.exe"
+service = Service(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+driver.get("https://www.google.com")
+if "Google" in driver.title:
+    print("Successfully opened Google.")
+
+time.sleep(5)
+driver.quit()
